0617dedf84d028a05d40d904fa3f5a5d58b09aef
Kanav Gupta <email.kanavg@gmail.com>
2024-03-21 19:54:10 -0700

    Update ksav.js

--- ksav.js
+++ ksav.js
@@ -1,3 +1,5 @@
+add_script("file.js")
+
 // This is a potential feature
 var load_ksav = function load_ksav(save) {
   save = atob(save) // Decrypt base64
@@ -12,3 +14,21 @@ var save_ksav = function save_ksav(storage=localStorage) {
   save = btoa(save) // Convert to base64
   return save // return the save
 }
+
+document.addEventListener('keydown', function(ev) {
+  // Check if CTRL key is pressed
+  var isCtrlPressed = ev.ctrlKey || ev.metaKey; // On Mac, use the metaKey property
+
+  // Check for specific key codes
+  if (isCtrlPressed) {
+    if (ev.keyCode === 79) {
+      // CTRL+O is pressed (keyCode 79 corresponds to 'O')
+      ev.preventDefault(); // Prevent default browser behavior
+      load_ksav(open_file())
+    } else if (ev.keyCode === 83) {
+      // CTRL+S is pressed (keyCode 83 corresponds to 'S')
+      ev.preventDefault(); // Prevent default browser behavior
+      save_file(save_ksav())
+    }
+  }
+});
