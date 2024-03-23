d18d721d2bbf9b902b269d2cd75255b7892b5b17
s0urce-c0de <email.kanavg@gmail.com>
2024-03-22 21:36:04 -0700

    pls work

--- ksav.js
+++ ksav.js
@@ -19,28 +19,7 @@ var load_ksav = function load_ksav(save) {
 }
 
 var save_ksav = function save_ksav(storage=localStorage) {
-  save = JSON.stringify(localStorage) // Save JSON -> string
+  save = JSON.stringify(storage) // Save JSON -> string
   save = btoa(save) // Convert to base64
   return save // return the save
-}
-var add_ksav_event_listeners = function() {
-  document.addEventListener('keydown', function(ev) {
-    // Check if CTRL key is pressed
-    var isCtrlPressed = ev.ctrlKey || ev.metaKey; // On Mac, use the metaKey property
-
-    // Check for specific key codes
-    if (isCtrlPressed) {
-      if (ev.keyCode === 79) {
-        // CTRL+O is pressed (keyCode 79 corresponds to 'O')
-        ev.preventDefault(); // Prevent default browser behavior
-        // clear localStorage
-        localStorage.clear()
-        open_file(load_ksav)
-      } else if (ev.keyCode === 83) {
-        // CTRL+S is pressed (keyCode 83 corresponds to 'S')
-        ev.preventDefault(); // Prevent default browser behavior
-        save_file(save_ksav())
-      }
-    }
-  });
 }
